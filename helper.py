dc31ca459097aae280067fd21dd1d1c039d33ea0
Xinyu Zhu <zhuxinyu@naf-atlas15.desy.de>
2023-08-07 11:04:34 +0200

    initial commit

---
+++ helper.py
@@ -0,0 +1,108 @@
+from ROOT import TCanvas, TLegend, TFile, TPaveLabel, TPaveText, TTree, TLorentzVector, TH1D, TVectorT, gStyle, THStack, gPad
+import ROOT
+import numpy as np
+
+
+
+def is_bjet(tag):
+    if tag ==  '\x01': return True
+    elif tag == '\x00': return False
+    else: 
+        print("warning: this tag is neither 0 nor 1")
+        return False
+    
+
+def GetLorenzP4List(tree, obj):
+    """
+    returns a list of LorenzP4 of a certain object (el, mu, bjet) for each event
+    """
+    p4s, __pts, __etas, __phis, __es = [], [], [], [], []
+    # read from the entry
+    
+    if obj == 'mu':
+        __pts = getattr(tree, "mu_pt")
+        __etas = getattr(tree, "mu_eta")
+        __phis = getattr(tree, "mu_phi")
+        __es = getattr(tree, "mu_e")
+    elif obj == 'el':
+        __pts = getattr(tree, "el_pt")
+        __etas = getattr(tree, "el_eta")
+        __phis = getattr(tree, "el_phi")
+        __es = getattr(tree, "el_e")
+    elif obj == 'bjet':
+        __tags = ROOT.vector('string')()
+        __tags = getattr(tree, "jet_isbtagged_DL1r_77")
+        __pts_all = getattr(tree, "jet_pt")
+        __etas_all = getattr(tree, "jet_eta")
+        __phis_all = getattr(tree, "jet_phi")
+        __es_all = getattr(tree, "jet_e")
+       
+        for i in range(len(__tags)):
+            if is_bjet(__tags[i]):
+            # if True: 
+                __pts.append(__pts_all[i])
+                __etas.append(__etas_all[i])
+                __phis.append(__phis_all[i])
+                __es.append(__es_all[i]) 
+    else:
+        print("this object is not defined in the function !")
+        return []
+    
+    # construct p4 and append it to list
+    for n in range(len(__pts)):
+        p4 = TLorentzVector()
+        p4.SetPtEtaPhiE(float(__pts[n]) ,float(__etas[n]) , float(__phis[n]) , float(__es[n]))
+        p4s.append(p4)
+    return p4s
+
+
+def W_to_which(p4s_el, p4s_mu):
+    """
+    returns the type of W decay 
+    (elel = 1, elmu = 2, mumu = 3, invalid event = 0)
+    """
+    # only select events with exactly 2 bjets
+    if len(p4s_el) == 2 and len(p4s_mu) == 0: return 1
+    elif len(p4s_el) == 1 and len(p4s_mu) == 1: return 2
+    elif len(p4s_el) == 0 and len(p4s_mu) == 2: return 3
+    else: return 0
+
+
+def getp4s_lep(p4s_el, p4s_mu):
+    """
+    merge muon/electron p4 together, dilep event only
+    """
+    p4s_lep = []
+    if W_to_which(p4s_el, p4s_mu): # is dilep event
+        p4s_lep.extend(p4s_el)
+        p4s_lep.extend(p4s_mu)
+        # print(len(p4s_lep))
+        if len(p4s_lep) != 2: print("error: not 2 leptons")
+    return p4s_lep
+
+def minimax(p4s_a, p4s_b):
+    """
+    get the index of minimax pair b+l, return index_a, index_b
+    a is the particle fixed for max a1b? in first step
+    b is the particle searched for max a1b? in first step
+    
+    """
+    masses = np.empty((2,2))
+    for i, p4_a in enumerate(p4s_a):
+        for j, p4_b in enumerate(p4s_b):
+            # get the max m_b?li, save the idx
+            masses[i,j] = (p4_a + p4_b).M()/1000
+    # use minimax method to get the pair:
+
+    # the indices of b with first and second lep (max(bl)):
+    indices = np.array([masses[0].argmax(),masses[1].argmax()])
+    # the index of l with minimum max
+    index2 = np.array([masses[0,indices[0]], masses[1,indices[1]]]).argmin()
+    # return the found index of 1st obj, 2nd obj
+    return index2, indices[index2] 
+    
+
+
+
+
+    
